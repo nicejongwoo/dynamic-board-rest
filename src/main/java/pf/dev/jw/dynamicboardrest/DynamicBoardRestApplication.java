be1533f7fbfa6ab5c80ca2ab4e74690ebd784dc8
leejongwoo <leejongwoo.dev@gmail.com>
2022-07-08 21:10:30 +0900

    Create project

---
+++ src/main/java/pf/dev/jw/dynamicboardrest/DynamicBoardRestApplication.java
@@ -0,0 +1,13 @@
+package pf.dev.jw.dynamicboardrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicBoardRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicBoardRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pf/dev/jw/dynamicboardrest/DynamicBoardRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pf.dev.jw.dynamicboardrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicBoardRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
