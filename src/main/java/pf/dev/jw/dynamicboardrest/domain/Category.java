e63d34fcb4e53b28f10cf3bac1d23298421d2432
leejongwoo <leejongwoo.dev@gmail.com>
2022-07-17 11:48:07 +0900

    Implements edit category api

--- src/main/java/pf/dev/jw/dynamicboardrest/domain/Category.java
+++ src/main/java/pf/dev/jw/dynamicboardrest/domain/Category.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import pf.dev.jw.dynamicboardrest.controller.dto.request.CategoryRequest;
 
 import javax.persistence.*;
 import java.util.List;
@@ -30,6 +31,12 @@ public class Category {
 
     @Builder
     public Category(String name, String code, Board board) {
+        this.name = name;
+        this.code = code;
+        this.board = board;
+    }
+
+    public void updateCategory(String name, String code, Board board) {
         this.name = name;
         this.code = code;
         this.board = board;
--- src/main/java/pf/dev/jw/dynamicboardrest/service/impl/CategoryServiceImpl.java
+++ src/main/java/pf/dev/jw/dynamicboardrest/service/impl/CategoryServiceImpl.java
@@ -31,11 +31,9 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Long register(CategoryRequest request) {
         //카테고리 코드 중복 체크
-        if (categoryRepository.existsByCode(request.getCode())) throw new CustomApiException("카테고리 코드가 이미 있습니다.", HttpStatus.BAD_REQUEST);
+        checkDuplCode(request);
         //게시판 존재 체크
-        Board board = boardRepository.findById(request.getBoardId()).orElseThrow(
-                () -> new CustomApiException("게시판이 없습니다.", HttpStatus.NOT_FOUND)
-        );
+        Board board = checkBoard(request);
 
         Category category = CategoryDtoMapper.MAPPER.toEntity(request, board);
         categoryRepository.save(category);
@@ -49,10 +47,8 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryResponse getOne(Long id) {
-        //카테고리존재여부
-        Category category = categoryRepository.findById(id).orElseThrow(
-                () -> new CustomApiException("카테고리가 없습니다.", HttpStatus.NOT_FOUND)
-        );
+        //카테고리 존재 체크
+        Category category = checkCategory(id);
         CategoryResponse response = CategoryDtoMapper.MAPPER.toDto(category);
         return response;
     }
@@ -60,6 +56,31 @@ public class CategoryServiceImpl implements CategoryService {
     @Transactional
     @Override
     public void edit(Long id, CategoryRequest request) {
+        //카테고리 코드 중복 체크
+        checkDuplCode(request);
+        //카테고리 존재 체크
+        Category category = checkCategory(id);
+        //게시판 존재 체크
+        Board board = checkBoard(request);
+        //업데이트
+        category.updateCategory(request.getName(), request.getCode(), board);
+    }
 
+    private Board checkBoard(CategoryRequest request) {
+        Board board = boardRepository.findById(request.getBoardId()).orElseThrow(
+                () -> new CustomApiException("게시판이 없습니다.", HttpStatus.NOT_FOUND)
+        );
+        return board;
+    }
+
+    private Category checkCategory(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(
+                () -> new CustomApiException("카테고리가 없습니다.", HttpStatus.NOT_FOUND)
+        );
+        return category;
+    }
+
+    private void checkDuplCode(CategoryRequest request) {
+        if (categoryRepository.existsByCode(request.getCode())) throw new CustomApiException("카테고리 코드가 이미 있습니다.", HttpStatus.BAD_REQUEST);
     }
 }
